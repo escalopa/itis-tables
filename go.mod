ee4da59737cf45e5bee2e84e9b65757738affa6c
escalopa <ahmad.helaly.dev@gmail.com>
2023-05-26 08:39:12 +0300

    create http server

--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,10 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/escalopa/itis-tables/internal/application"
 )
@@ -19,5 +23,41 @@ func New(ctx context.Context, uc *application.UseCase) *Server {
 }
 
 func (s *Server) Run(port string) error {
+	http.HandleFunc("/table", s.tableHandler)
+
+	// Start the server
+	log.Printf("Starting server on port %s...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return nil
+}
+
+func (s *Server) tableHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the query parameters from the request
+	group := r.URL.Query().Get("group")
+	dateString := r.URL.Query().Get("date")
+
+	// Parse the date string into a time.Time value
+	date, err := time.Parse("02/01/2006", dateString)
+	if err != nil {
+		http.Error(w, "Invalid date format", http.StatusBadRequest)
+		return
+	}
+
+	if group == "" {
+		http.Error(w, "Missing group parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Call your function with the provided parameters
+	tables, err := s.uc.GetSchedule(r.Context(), group, date)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the result to the response
+	fmt.Fprintf(w, "Result: %s", tables)
 }
