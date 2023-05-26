9c950052aeb451cd817ee378f2af8063b1ff530b
escalopa <ahmad.helaly.dev@gmail.com>
2023-05-26 08:08:24 +0300

    impl usecase to fetch tables

--- internal/application/ports.go
+++ internal/application/ports.go
@@ -5,26 +5,22 @@ import (
 	"time"
 
 	"github.com/escalopa/itis-tables/core"
+	"golang.org/x/net/html"
 )
 
 type TableParser interface {
-	PraseTable(ctx context.Context, tablesPath string) error
-}
-
-type CourseParser interface {
-	ParseCourses(ctx context.Context, coursesPath string) error
+	PraseTable(ctx context.Context, doc *html.Node) (map[time.Weekday][][]core.Subject, error)
 }
 
 type TableRepository interface {
-	GetSchedule(ctx context.Context, group string, day time.Weekday) ([]core.Subject, error)
-	SetShedule(ctx context.Context, group string, day time.Weekday, subjects []core.Subject) error
-}
-
-type CourseRepository interface {
-	GetCourse(ctx context.Context, studentID string) ([]core.Course, error)
-	SetCourse(ctx context.Context, studentID string, course []core.Course) error
+	GetSchedule(ctx context.Context, group string, day time.Weekday) ([][]core.Subject, error)
+	SetShedule(ctx context.Context, group string, day time.Weekday, subjects [][]core.Subject) error
 }
 
 type EvenOddDate interface {
 	GetWeek(ctx context.Context, now time.Time) core.WeekType
+}
+
+type PageFetcher interface {
+	FetchTablePage(ctx context.Context, group string, date string) (*html.Node, error)
 }
--- internal/application/usecases.go
+++ internal/application/usecases.go
@@ -2,13 +2,15 @@ package application
 
 import (
 	"context"
+	"time"
 
 	"github.com/escalopa/itis-tables/core"
 )
 
 type UseCase struct {
 	tr  TableRepository
-	cr  CourseRepository
+	tp  TableParser
+	pf  PageFetcher
 	eod EvenOddDate
 }
 
@@ -26,9 +28,15 @@ func WithTableRepository(tr TableRepository) func(*UseCase) {
 	}
 }
 
-func WithCourseRepository(cr CourseRepository) func(*UseCase) {
+func WithTableParser(tp TableParser) func(*UseCase) {
 	return func(uc *UseCase) {
-		uc.cr = cr
+		uc.tp = tp
+	}
+}
+
+func WithPageFetcher(pf PageFetcher) func(*UseCase) {
+	return func(uc *UseCase) {
+		uc.pf = pf
 	}
 }
 
@@ -38,6 +46,31 @@ func WithEvenOddDate(eod EvenOddDate) func(*UseCase) {
 	}
 }
 
-func (uc *UseCase) GetSchedule() ([]core.Subject, error) {
-	return nil, nil
+func (uc *UseCase) GetSchedule(ctx context.Context, group string, date time.Time) ([][]core.Subject, error) {
+	// Check if data already stored in cache
+	if subjects, err := uc.tr.GetSchedule(ctx, group, date.Weekday()); err == nil {
+		return subjects, nil
+	}
+
+	// Fetch the page and parse it
+	d := date.Format("02.01.2006")
+	body, err := uc.pf.FetchTablePage(ctx, group, d)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the table
+	tables, err := uc.tp.PraseTable(ctx, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Store in cache
+	go func(tables map[time.Weekday][][]core.Subject) {
+		for i := range core.DaysInOrder {
+			uc.tr.SetShedule(ctx, group, core.DaysInOrder[i], tables[core.DaysInOrder[i]])
+		}
+	}(tables)
+
+	return tables[date.Weekday()], nil
 }
